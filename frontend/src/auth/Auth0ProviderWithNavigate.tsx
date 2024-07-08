caf8dd91b03ddd487c3d0373d50c1c5de6ab16d8
parasd086 <parasd086@gmail.com>
2024-07-08 15:50:09 +0530

    Add create user request from frontend

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,6 +1,7 @@
 //here, code that uses Auth0 SDK to connect to our Auth0 a/c
 
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useCreateMyUser } from "../api/MyUserApi";
 
 type Props = {
   children: React.ReactNode;
@@ -8,6 +9,8 @@ type Props = {
 
 //this provider will wrap all the components in our app that needs Auth0 stuff
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -19,7 +22,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   //this function gets called when the user gets redirected back to our app
   //appState- is going to contain some stored data that we might need when the user gets redirected back
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("User- ", user);
+    console.log(user);
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
 
   return (
