98d0d6412788d6997e203601da4d3236a4c1e788
parasd086 <parasd086@gmail.com>
2024-07-08 12:08:59 +0530

    Integrate Auth0 to frontend

---
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,39 @@
+//here, code that uses Auth0 SDK to connect to our Auth0 a/c
+
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+//this provider will wrap all the components in our app that needs Auth0 stuff
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+  if (!domain || !clientId || !redirectUri) {
+    throw new Error("Unable to initialise auth");
+  }
+
+  //this function gets called when the user gets redirected back to our app
+  //appState- is going to contain some stored data that we might need when the user gets redirected back
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    console.log("User- ", user);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithNavigate;
--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,10 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+  //loginWithRedirect= going to send the user to the hosted register/login page
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <Button
       variant="ghost"
       className="font-bold hover:text-orange-500 hover:bg-white"
+      onClick={async () => await loginWithRedirect()}
     >
       Log In
     </Button>
