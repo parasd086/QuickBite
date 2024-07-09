53c7fd43940aba1f3bb4889eabda1733abdf0abf
parasd086 <parasd086@gmail.com>
2024-07-09 14:59:00 +0530

    Add backend service to render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Health-endpoint
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "Health OK!" });
+});
+
 app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
