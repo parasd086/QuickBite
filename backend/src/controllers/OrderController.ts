6ac2f4d5737250e8b49155e145b00ed25256520f
parasd086 <parasd086@gmail.com>
2024-07-13 11:41:58 +0530

    Add orders to checkout api and stripe cli

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Stripe from "stripe";
 import Restaurant, { MenuItemType } from "../models/restaurant";
+import Order from "../models/order";
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -21,6 +22,13 @@ type CheckoutSessionRequest = {
   restaurantId: string;
 };
 
+const stripeWebhookHandler = async (req: Request, res: Response) => {
+  console.log("RECEIVED EVENT");
+  console.log("=============");
+  console.log("event:", req.body);
+  res.send();
+};
+
 const createCheckoutSession = async (req: Request, res: Response) => {
   try {
     //cart details and user details
@@ -34,14 +42,14 @@ const createCheckoutSession = async (req: Request, res: Response) => {
       throw new Error("Restaurant not found");
     }
 
-    // const newOrder = new Order({
-    //   restaurant: restaurant,
-    //   user: req.userId,
-    //   status: "placed",
-    //   deliveryDetails: checkoutSessionRequest.deliveryDetails,
-    //   cartItems: checkoutSessionRequest.cartItems,
-    //   createdAt: new Date(),
-    // });
+    const newOrder = new Order({
+      restaurant: restaurant,
+      user: req.userId,
+      status: "placed",
+      deliveryDetails: checkoutSessionRequest.deliveryDetails,
+      cartItems: checkoutSessionRequest.cartItems,
+      createdAt: new Date(),
+    });
 
     //LineItems = the left hand side stuff you see about our order and subtotal and stuff.
     const lineItems = createLineItems(
@@ -51,7 +59,7 @@ const createCheckoutSession = async (req: Request, res: Response) => {
 
     const session = await createSession(
       lineItems,
-      "TEST_ORDER_ID",
+      newOrder._id.toString(), //even thou we haven't saved it to DB yet, mongoose have created an ID for us to use.
       restaurant.deliveryPrice,
       restaurant._id.toString()
     );
@@ -60,7 +68,7 @@ const createCheckoutSession = async (req: Request, res: Response) => {
       return res.status(500).json({ message: "Error creating stripe session" });
     }
 
-    //await newOrder.save();
+    await newOrder.save();
     res.json({ url: session.url });
   } catch (error: any) {
     console.log(error);
@@ -135,4 +143,5 @@ const createSession = async (
 
 export default {
   createCheckoutSession,
+  stripeWebhookHandler,
 };
